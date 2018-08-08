70d45f0040b744f1745cf41a90459879179e864c
Pablo Mendez <mendez.developer@gmail.com>
2018-08-08 18:44:50 -0300

    added mongoose, users, and auth routes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
+const keys = require('./config/keys');
 const app = express();
+
+require('./models/User');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+});
 
 app.use(bodyParser.json());
 
-require('./routes/authRoutes').default(app);
+require('./routes/authRoutes')(app);
 
 app.get("/", (req, res) => {
   res.send({ message: "THIS IS HOME" });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
 module.exports = app => {
-  app.post('/api/auth/login', (req, res) => {
-    console.log('[post - login]: ', req.body);
-    return res.send(req.body)
+  app.post('/api/auth/login', async (req, res) => {
+    const {
+      username,
+      email,
+      password
+    } = req.body
+
+    try {
+      const newUser = await User.create({
+        username,
+        email,
+        password
+      })
+  
+      return res.send(newUser);
+    } catch (err) {
+      return res.status(500).send({ message: 'There was an error creating a user'});
+    }    
   })
 
   app.get("/api/auth/current_user", (req, res) => {
